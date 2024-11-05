13ef18de93f5474cd66b174569e1a05c1f7aa77c
achref-21 <76564010+achref-21@users.noreply.github.com>
2024-11-05 16:27:51 +0100

    Update testStockService.java

--- 5DS3_G1_devopsSpring/src/main/test/tn/esprit/devopsSpring_5DS3_G1/testStockService.java
+++ 5DS3_G1_devopsSpring/src/main/test/tn/esprit/devopsSpring_5DS3_G1/testStockService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-//Taaaaaz
+//Taaaaaaz
 
 class StockServiceImplTest {
 
